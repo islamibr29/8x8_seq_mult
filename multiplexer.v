123c961bd2cf1b51f6101e534753f23f6e5abfa8
Islam Ibrahim <49861069+islamibr@users.noreply.github.com>
2023-07-13 18:51:49 +0300

    Create multiplexer.v

---
+++ multiplexer.v
@@ -0,0 +1,47 @@
+module mux4 (
+input [3:0] mux_in_a, mux_in_b,
+input [1:0] mux_sel,
+output reg [3:0] mux_out
+);
+
+always @ (*)
+begin
+	if (mux_sel == 0) begin
+	mux_out = mux_in_a;
+	end
+	else begin
+	mux_out = mux_in_b;
+	end
+end
+
+endmodule
+
+// testbench
+module Mux_TestBench();
+ reg [7:0] dataa;
+ reg [7:0] datab;
+ reg [1:0] selector;
+ wire [3:0] aout;
+ 
+ Mux mux(
+ .dataa(dataa),
+ .datab(datab),
+ .selector(selector),
+ .aout(aout),
+ );
+
+initial begin
+ 
+ dataa = 8'b 1111_0000;
+ datab = 8'b 1100_0011;
+   selector = 2'b 00;
+   #10
+   selector = 2'b 01;
+   #10
+   selector = 2'b 10;
+   #10
+   selector = 2'b 11;
+   
+end
+
+endmodule
